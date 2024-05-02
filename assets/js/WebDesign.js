711272df76037e9f32e8d5faaa07a39003ac1fee
Zaracruz <157723816+Zaracruz@users.noreply.github.com>
2024-05-02 14:29:00 +0100

    webdesignpage (#2)

--- assets/js/WebDesign.js
+++ assets/js/WebDesign.js
@@ -9,3 +9,8 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
   }
+
+  //button
+  function showAlert() {
+    alert("JavaScript function example");
+}
